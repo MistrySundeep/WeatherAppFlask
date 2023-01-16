0bd6b89604d6e3c6fb2c02cc333761b845abe501
Sundeep <mistrysundeep@gmail.com>
2023-01-16 15:15:13 +0000

    Adjusted get_weather endpoint to take a postcode from the user and return weather for that location

--- app/app.py
+++ app/app.py
@@ -14,18 +14,40 @@ def home():
     return '<h1>Hello World</h1>'
 
 
-@app.route('/get_weather')
-def get_weather_details():
-    x = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={settings.weather_api_key}')
-    print(x.json())
-    return x.json()
-
-
 @app.route('/address_lookup')
 def get_address_json():
     # Returns a json object that gives the users address and lon and lat values, uses a postcode and door number
     x = requests.get(f'https://api.getAddress.io/find/nw21qd/45?api-key={settings.address_api_key}')
+    x_json = x.json()
 
-    # Returns a json object that gives
-    y = requests.get(f'https://api.getAddress.io/find/nw118tn?api-key={settings.address_api_key}')
-    return x.json()
+    results = {
+        'lat': x_json.get('latitude'),
+        'lon': x_json.get('longitude')
+    }
+    # Returns a json object that gives the users address, lon and lat values using only a postcode
+    # y = requests.get(f'https://api.getAddress.io/find/nw118tn?api-key={settings.address_api_key}')
+    return results
+
+
+# To add variable use <type:name> as seen below
+@app.route('/get_weather/<string:postcode>')
+def get_weather_details(postcode: str):
+    # Format the postcode
+    print(postcode.lower().replace(" ", ""))
+
+    # Get address details
+    address_coordinates = requests.get(f'https://api.getAddress.io/find/{postcode}?api-key={settings.address_api_key}').json()
+
+    # Create a dict that will hold the lat and lon of the address
+    coordinate_dict = {
+        'lat': address_coordinates.get('latitude'),
+        'lon': address_coordinates.get('longitude')
+    }
+
+    # Make a request for the weather of this location using the dict values
+    # units=metric for degrees values
+    # cnt=1 sets the number of records returned, default is 20?
+    weather = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={coordinate_dict.get("lat")}&lon={coordinate_dict.get("lon")}&units=metric&cnt=1&appid={settings.weather_api_key}').json()
+    print(weather)
+
+    return weather
--- app/dict_work.py
+++ app/dict_work.py
@@ -1,14 +1,20 @@
-results = {
-    "addresses": [
-        "45 Cheviot Gardens, , , , , London, "
-    ],
-    "latitude": 51.5649457,
-    "longitude": -0.2114287
-}
+# results = {
+#     "addresses": [
+#         "45 Cheviot Gardens, , , , , London, "
+#     ],
+#     "latitude": 51.5649457,
+#     "longitude": -0.2114287
+# }
+#
+# lat = results.get('latitude')
+# lon = results.get('longitude')
+#
+# print(lat, lon)
+#
+#
 
-lat = results.get('latitude')
-lon = results.get('longitude')
 
-print(lat, lon)
+word = 'NW118TN'
 
+print(word.lower().replace(" ", ""))
 
